39bc4640ffacccd6a34ff5a1236b5f8bd416e9a0
Brett Sevy <justbrettt@gmail.com>
2020-02-24 21:35:05 -0600

    sql database written, connected to cli.js prompts working, and returning employee table

--- cli.js
+++ cli.js
@@ -4,15 +4,141 @@ const consoleT = require("console.table");
 
 
 let connection = mysql.createConnection({
-  host: "localhost",
-
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "employee_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees?",
+                "View all employees by deparment?",
+                "View all employees by manager?",
+                "Add employee?",
+                "Remove employee?",
+                "Update employee role?",
+                "Update employee manager?",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all employees?":
+                    viewEmployees();
+                    break;
+
+                case "View all employees by deparment?":
+                    viewByDepartment();
+                    break;
+
+                case "View all employees by manager?":
+                    viewByManager();
+                    break;
+
+                case "Add employee?":
+                    addEmployee();
+                    break;
+
+                case "Remove employee?":
+                    removeEmployee();
+                    break;
+
+                case "Update employee role?":
+                    updateRole();
+                    break;
+
+                case "Update employee manager?":
+                    updateManager();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
+
+function viewEmployees() {
+    connection.query("select * from employee", function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+    });
+    start();
+};
+
+function viewByDepartment() {
+    connection.query("select * from department", function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+    });
+    start();
+};
+
+function viewByManager() {
+    connection.query("select * from manager", function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+    });
+    start();
+
+};
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employees first name?",
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employees last name?",
+            },
+            {
+                type: "input",
+                name: "role_id",
+                message: "What is the employees role id number?",
+            },
+            {
+                type: "input",
+                name: "manader_id",
+                message: "What is the employees mamanger id number?",
+            },
+
+        ])
+
+    return start();
+
+};
+
+function removeEmployee() {
+    console.log("delete employee")
+};
+
+function updateRole() {
+    console.log("update role")
+};
+
+function updateManager() {
+    console.log("update manager")
+};
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -3,20 +3,38 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-  id INT(11) AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
+insert into department (name) values
+("sewing"),
+("patching"),
+("design"),
+("finance"),
+("accounting");
 
 
 CREATE TABLE role(
-  id INT(11) AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30),
   salary DECIMAL(10, 2),
   department_id INT,
-  PRIMAMRY KEY (id)
+  PRIMARY KEY (id)
 );
+
+insert into role(department_id, title, salary)
+values
+(1, "sewer", 30000),
+(1, "chief sewer", 50000),
+(2, "patcher", 20000),
+(3, "chief designer", 40000),
+(3, "designer", 25000),
+(4, "CFO", 250000),
+(4, "analyst", 25000),
+(5, "accountant", 60000),
+(5, "junior accountant", 40000);
 
 
 
@@ -28,3 +46,12 @@ CREATE TABLE employee(
   manager_id INT,
   PRIMARY KEY (id)
 );
+
+insert into employee(first_name, last_name, role_id, manager_id)
+values
+("Brett", "Sevy", 3, 4),
+("Chance", "Payne", 2, 3),
+("Chris", "Christensen", 5, 4),
+("Henry", "Jerman", 4, 5),
+("Sara", "Sevy", 2, 1),
+("Vivian", "Sevy", 1, 1);
